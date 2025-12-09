85718254bea3005555cf8e79b0ee50aa93f7bf6c
HGGshiwo <3190102788@zju.edu.cn>
2025-12-09 10:11:59 +0800

    增加string和number的参数控制

--- src/components/dialog/NodeDialog.tsx
+++ src/components/dialog/NodeDialog.tsx
@@ -8,21 +8,36 @@ export default async function promptNode(cfg: any) {
 
   return getJSON("/get_ros_param/mavproxy/other/node_cfg")?.then((res: any) => res["msg"])
     .then(async (topic_cfg: any[]) => {
-      const childCfg = await Promise.all(topic_cfg.map(({ topic, title }) => {
+      const childCfg = await Promise.all(topic_cfg.map(({ topic, title, type }) => {
         return getJSON(`/get_ros_param${topic}`)?.then(res => {
           const value = res["status"] == "error" ? undefined : res["msg"]
-          return { value, topic, title }
+          return { value, topic, title, type }
         })
       }))
       const _c: Record<string, any> = {}
-      childCfg.filter(v => (!!v && v.value != undefined)).forEach(({ value, topic, title }: any) => {
+      childCfg.filter(v => (!!v && v.value != undefined)).forEach(({ value, topic, title, type }: any) => {
         defaultValue = { ...defaultValue, [topic]: value }
-        _c[topic] = (value1: boolean, onChange: any) => (
-          <label className="label min-w-xs max-w-full">
-            <input type="checkbox" checked={value1} onChange={(e) => onChange(e.target.checked)} className="toggle" />
-            {title}
-          </label>
-        )
+        const func_map = {
+          "boolean": (value1: boolean, onChange: any) => (
+            <label className="label min-w-xs max-w-full">
+              <input type="checkbox" checked={value1} onChange={(e) => onChange(e.target.checked)} className="toggle" />
+              {title}
+            </label>
+          ),
+          "number": (value1: number, onChange: any) => (
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">{title}</legend>
+              <input type="number" value={value1} onChange={(e) => onChange(e.target.value)} className="input" />
+            </fieldset>
+          ),
+          "string": (value1: string, onChange: any) => (
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">{title}</legend>
+              <input type="text" value={value1} onChange={(e) => onChange(e.target.value)} className="input" />
+            </fieldset>
+          )
+        }
+        _c[topic] = func_map[type as "string" | "number" | "boolean"]
       })
       child = _c
     }).then(() => {
