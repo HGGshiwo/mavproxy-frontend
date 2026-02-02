5e5458f1624bef339c459174dd36bd4111b91cc5
HGGshiwo <3190102788@zju.edu.cn>
2026-02-02 10:30:00 +0800

    修改输入方式

--- src/components/dialog/ExposureDialog.tsx
+++ src/components/dialog/ExposureDialog.tsx
@@ -1,4 +1,5 @@
-import { getJSON, handleInputChange, postJSON } from '../../utils'
+import { InputNumber } from 'antd'
+import { getJSON, postJSON } from '../../utils'
 import { Toast } from '../Toast'
 import { openPrompt } from './BaseDialog'
 
@@ -18,8 +19,9 @@ export default async function promptExposure(cfg: any) {
         <fieldset className="fieldset">
           <legend className="fieldset-legend">快门时间</legend>
           <div className='flex flex-row justify-center items-center'>
-            <input type="range" min={shutter.min} max={shutter.max} value={value1} className="range range-neutral" onChange={(e) => onChange(e.target.value)} />
-            <span className='pl-1'>{value1}</span>
+            {/* <input type="range" min={shutter.min} max={shutter.max} value={value1} className="range range-neutral" onChange={(e) => onChange(e.target.value)} />
+            <span className='pl-1'>{value1}</span> */}
+            <InputNumber mode="spinner" step={shutter.step} min={shutter.min} max={shutter.max} value={value1} variant="filled" onChange={(e) => onChange(e)} placeholder="Filled" />
           </div>
 
         </fieldset>
@@ -28,8 +30,9 @@ export default async function promptExposure(cfg: any) {
         <fieldset className="fieldset">
           <legend className="fieldset-legend">感光度</legend>
           <div className='flex flex-row justify-center items-center'>
-            <input type="range" min={sensitivity.min} max={sensitivity.max} value={value1} className="range range-neutral" onChange={(e) => onChange(e.target.value)} />
-            <span className='pl-1'>{value1}</span>
+            {/* <input type="range" min={sensitivity.min} max={sensitivity.max} value={value1} className="range range-neutral" onChange={(e) => onChange(e.target.value)} />
+            <span className='pl-1'>{value1}</span> */}
+            <InputNumber mode="spinner" step={sensitivity.step} min={sensitivity.min} max={sensitivity.max} value={value1} variant="filled" onChange={(e) => onChange(e)} placeholder="Filled" />
           </div>
         </fieldset>
       )
